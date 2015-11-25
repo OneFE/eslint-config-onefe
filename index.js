635437899531e96d31dd7ac507802d154a713384
luqin <h-io@qq.com>
2015-11-25 22:12:43 +0800

    add rules

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ module.exports = {
   'extends': [
     'eslint-config-airbnb'
   ],
-  rules: {}
+  rules: {
+    "id-length": 0,
+    "prefer-const": 0,
+    "react/jsx-quotes": 0,
+    "jsx-quotes": [2, "prefer-double"]
+  }
 };
