54c3eab5358ed765ae6f62beaf4add154a382a18
luqin <luqin2006@163.com>
2015-11-27 18:05:31 +0800

    update

--- base.js
+++ base.js
@@ -1,6 +1,11 @@
-module.exports = {
-  'extends': [
-    'eslint-config-airbnb/base'
-  ],
-  rules: {}
-};
+var deepAssign = require('deep-assign');
+var airbnb = require('eslint-config-airbnb/base');
+
+module.exports = deepAssign(airbnb, {
+  globals: {
+  },
+  rules: {
+    'id-length': 0,
+    'prefer-const': 0
+  }
+});
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-module.exports = {
-  'extends': [
-    'eslint-config-airbnb'
-  ],
+var deepAssign = require('deep-assign');
+var airbnb = require('eslint-config-airbnb');
+
+module.exports = deepAssign(airbnb, {
+  globals: {
+  },
   rules: {
-    "id-length": 0,
-    "prefer-const": 0,
-    "react/jsx-quotes": 0,
-    "jsx-quotes": [2, "prefer-double"]
+    'id-length': 0,
+    'prefer-const': 0,
+    'react/jsx-quotes': 0,
+    'jsx-quotes': [2, 'prefer-double']
   }
-};
+});
